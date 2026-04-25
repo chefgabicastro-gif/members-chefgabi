7b8c31301f12d9dd0a6d378d24a41ec39458bec7
rickstarl <ribeiro.freire3@gmail.com>
2026-04-25 09:13:04 -0300

    fix: serve dashboard on app custom domain

--- apps/web/src/dev-server.js
+++ apps/web/src/dev-server.js
@@ -8,8 +8,21 @@ const __dirname = path.dirname(__filename);
 const publicDir = path.resolve(__dirname, "../public");
 const app = express();
 const PORT = process.env.WEB_PORT || 3000;
+const APP_HOSTS = new Set(["app.escoladeproposito.online"]);
 
 app.use(express.static(publicDir));
+
+app.get("/", (req, res, next) => {
+  const host = String(req.headers.host || "")
+    .replace(/:\d+$/, "")
+    .toLowerCase();
+
+  if (APP_HOSTS.has(host)) {
+    return res.sendFile(path.join(publicDir, "dashboard.html"));
+  }
+
+  return next();
+});
 
 app.get("*", (_req, res) => {
   res.sendFile(path.join(publicDir, "index.html"));
@@ -19,4 +32,3 @@ app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Members Web running on port ${PORT}`);
 });
-
